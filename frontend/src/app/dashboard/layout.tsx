5d00e5cc0fc2643cebd3b37fac5660f90eb4f84d
shafiulla <shafishafi57428@gmail.com>
2026-07-15 13:19:11 +0530

    feat: Remove Messages tab from Teacher Portal

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -376,15 +376,6 @@ export default function DashboardLayout({
           ),
         },
         {
-          name: 'Messages',
-          href: '/dashboard/communication',
-          svg: (
-            <svg className="icon-svg" viewBox="0 0 24 24">
-              <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-            </svg>
-          ),
-        },
-        {
           name: 'Calendar',
           href: '/dashboard/calendar',
           svg: (
